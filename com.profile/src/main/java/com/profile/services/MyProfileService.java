8d911b2f83636b03d1c508fef4ad2126b85164dc
Ankit-Raj-2721 <ankit.raj.arv@gmail.com>
2024-12-17 18:26:45 +0530

    Committing the whole project.

---
+++ com.profile/src/main/java/com/profile/services/MyProfileService.java
@@ -0,0 +1,9 @@
+package com.profile.services;
+
+import com.profile.entity.MyProfile;
+
+public interface MyProfileService {
+
+	void createProfile(MyProfile profile);
+
+}
---
+++ com.profile/src/main/java/com/profile/services/MyProfileServiceImplementation.java
@@ -0,0 +1,20 @@
+package com.profile.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.profile.entity.MyProfile;
+import com.profile.repositories.MyProfileRepository;
+
+@Service
+public class MyProfileServiceImplementation implements MyProfileService {
+	
+	@Autowired
+	MyProfileRepository repo;
+	
+	@Override
+	public void createProfile(MyProfile profile) {
+		repo.save(profile);
+	}
+
+}
